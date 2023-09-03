abdb25b42e4972f1d4aeb16508b3eaef2c719371
Meet1611 <140803389+Meet1611@users.noreply.github.com>
2023-09-03 13:06:00 +0530

    Add files via upload

--- Verification of Triangle.c
+++ Verification of Triangle.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 int main () {
 	int a,b,c;
-	printf("Enter Height of Triangle ");
+	printf("Enter base of Triangle : ");
 	scanf("%d",&a);
-	printf("Enter Base of Triangle ");
+	printf("Enter height of Triangle : ");
 	scanf("%d",&b);
-	printf("Enter Hypotenuse of Triangle ");
+	printf("Enter hypotenuse of Triangle : ");
 	scanf("%d",&c);
-	if(a==b && b==c && c==a) {
+	if (a==b && b==c) {
 		printf("Triangle is Equilateral");
 	}
 	else {
-			if (a==b || b==c ||c==a) {
-		printf("Triangle is Isosceles");
-	        }
-	        else if ( ((a*a)+(b*b)) == (c*c) ) {
-	        	printf("Triangle is Scalene");
-			}
+		if (a==b || b==c||a==c) {
+			printf("Triangle is Isosceles");
+		}
+		else if ((a*a)+(b*b)==(c*c)) {
+			printf("Trianle is Scalene");
+		}
 	}
 	return 0;
-} 
+}
