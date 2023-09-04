af3b4c735b3c22e76b6a293dd4007c87454ba645
Meet1611 <140803389+Meet1611@users.noreply.github.com>
2023-09-04 17:23:52 +0530

    Add files via upload

---
+++ Entered character is alphabet or special character.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main () {
+	char c;
+	printf("Enter any character : ");
+	scanf("%c",&c);
+	(c>='A'||c<='Z'||c>='a'||c<='z')?printf("%c is alphabet",c):printf("%c is special character",c);
+	return 0;
+}
