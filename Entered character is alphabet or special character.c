4467650cb022381e5dcf8ce0e37c93b99fcc9ef3
Meet1611 <140803389+Meet1611@users.noreply.github.com>
2023-09-23 23:06:29 -0700

    Update Entered character is alphabet or special character.c

--- Entered character is alphabet or special character.c
+++ Entered character is alphabet or special character.c
@@ -3,6 +3,6 @@ int main () {
 	char c;
 	printf("Enter any character : ");
 	scanf("%c",&c);
-	(c>='A'||c<='Z'||c>='a'||c<='z')?printf("%c is alphabet",c):printf("%c is special character",c);
+	((c>='A'&& c<='Z')||(c>='a'&& c<='z'))?printf("%c is alphabet",c):printf("%c is special character",c);
 	return 0;
 }
